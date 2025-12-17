fe7955b83a3f81455882381b43521492135e4587
somuraj07 <somasankar33@gmail.com>
2025-12-17 13:05:59 +0530

    team

--- components/team.tsx
+++ components/team.tsx
@@ -148,7 +148,7 @@ export default function TeamSection() {
                   alt={member.name}
                   width={400}
                   height={400}
-                  className="h-[220px] sm:h-[260px] lg:h-[300px] w-full object-cover transition-transform duration-300 group-hover:scale-[1.03]"
+                  className="h-[450px] sm:h-[300px] lg:h-[300px] w-full object-cover transition-transform duration-300 group-hover:scale-[1.03]"
                 />
               </div>
 
